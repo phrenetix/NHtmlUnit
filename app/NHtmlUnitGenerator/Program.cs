4caf149f42339af8579a26390e3b3d3e4af18c3a
phrenetix <erik.andreassen.perez@gmail.com>
2011-12-16 15:57:42 +0100

    Added handling of properties. Not been able to test it yet, but looks fine so far.

--- app/NHtmlUnitGenerator/Program.cs
+++ app/NHtmlUnitGenerator/Program.cs
@@ -10,7 +10,11 @@
 #endregion
 
 using System;
-
+using System.Collections;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using Mono.Collections.Generic;
 using com.gargoylesoftware.htmlunit;
 
 using Mono.Cecil;
@@ -47,6 +51,72 @@ namespace NHtmlUnit.Generator
 
             AssemblyDefinition htmlUnitAssembly =
                 AssemblyDefinition.ReadAssembly(fileName, parameters);
+
+            //Iterate through all classes
+
+     
+
+            foreach (var typeDefinition in htmlUnitAssembly.MainModule.Types.Where(x => x.Namespace.StartsWith("com.gargoylesoftware.htmlunit") && x.IsPublic))
+            {
+
+                IDictionary<MethodDefinition, MethodDefinition> properties = null;
+
+                Console.WriteLine("\tClass: {0}", typeDefinition.Name);
+                //Find all the getters and isses.
+                
+                var publicMethods = typeDefinition.Methods.Where(x => x.IsPublic);
+                if (publicMethods.Count() > 0)
+                {
+                     properties = new ConcurrentDictionary<MethodDefinition, MethodDefinition>();
+                     IEnumerable<MethodDefinition> getters = publicMethods.Where(x => x.Name.StartsWith("get") || x.Name.StartsWith("is") && x.Parameters.Count == 0);
+                     IEnumerable<MethodDefinition> setters = publicMethods.Where(x => x.Name.StartsWith("set") && x.Parameters.Count > 0);
+
+                    IList<PropertyDefinition> propertyDefinitions = new Collection<PropertyDefinition>();
+                     
+                    foreach (var getMethod in getters)
+                    {
+                        string name = null;
+                        name = getMethod.Name.Remove(getMethod.Name.StartsWith("get") ? 2 : 1);
+                        MethodDefinition setMethod = setters.FirstOrDefault(x => x.Name.EndsWith(name));
+                        properties.Add(new KeyValuePair<MethodDefinition, MethodDefinition>(getMethod, setMethod));
+                    }
+
+
+                    foreach (KeyValuePair<MethodDefinition, MethodDefinition> property in properties)
+                    {
+                        string name = property.Key.Name.Remove(2);
+                        PropertyDefinition definition = new PropertyDefinition(name, PropertyAttributes.HasDefault, property.Key.ReturnType);
+                        definition.GetMethod = property.Key;
+                        definition.SetMethod = property.Value;
+                        propertyDefinitions.Add(definition);
+
+                    }
+
+                    foreach(var prop in propertyDefinitions)
+                        typeDefinition.Properties.Add(prop);
+
+                    /* Console.WriteLine("\t\tGetters && Isses: ");
+                     foreach (
+                         var methodDefinition in
+                             publicMethods.Where(
+                                 x => x.Name.StartsWith("get") || x.Name.StartsWith("is") && x.Parameters.Count == 0))
+                     {
+                         Console.WriteLine("\t\t\t{0}", methodDefinition.Name);
+                     }
+                    
+                     Console.WriteLine("\t\tSetters: ");
+                     foreach (
+                         var methodDefinition in
+                             publicMethods.Where(x => x.Name.StartsWith("set") && x.Parameters.Count > 0))
+                     {
+                         Console.WriteLine("\t\t\t{0}", methodDefinition.Name);
+                     }
+                     * */
+                }
+
+                
+
+            }
         }
     }
 }
